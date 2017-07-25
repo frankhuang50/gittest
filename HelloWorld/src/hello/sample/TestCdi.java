2015964e07dab3289fe8b58d5000321349dc027e
frankhuang50 <x164433@L028957.corp.ads>
2017-07-25 09:18:01 -0700

    commit

---
+++ HelloWorld/src/hello/sample/TestCdi.java
@@ -0,0 +1,31 @@
+package hello.sample;
+
+import javax.ejb.EJB;
+import javax.inject.Inject;
+
+
+
+public class TestCdi {
+	
+
+	
+//	@Inject
+//	TestEjb te;
+	
+	@Inject
+	TestCdiUtility tc;
+public void sayHello() {
+	tc.sayHelloFromUtility();
+}
+
+
+
+
+//@Inject=
+//TestCdiUtility tc;
+//	public void sayHelloFromUtility() {
+//		//tc.sayHelloFromUtility();
+//		TestCdiUtility tc2 = new TestCdiUtility();
+//		tc2.sayHelloFromUtility();
+// }
+}
